5b99531e7e457668367c8c8dab90361c6f4f4f11
Oscar Giraldo <oscardan@utp.edu.co>
2020-04-20 19:51:58 -0500

    Modificando Nuestra Clase main

--- src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
+++ src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
@@ -2,12 +2,25 @@ package co.edu.utp.isc.gia.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("hello")
 public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
+        
+        @GetMapping("/{firsName}/{lastName}")
+        public String sayHello(
+                @PathVariable("firstName") String firstName,
+                @PathVariable("firstName") String lastName) {
+            return String.format("Hello  %s %s", firstName, lastName);
+        }
 
 } 
