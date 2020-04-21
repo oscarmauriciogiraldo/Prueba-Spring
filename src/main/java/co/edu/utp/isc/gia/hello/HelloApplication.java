1492230f58fbcf21a9f2acaa07d86a6fc7da8a6b
Oscar Giraldo <oscardan@utp.edu.co>
2020-04-20 22:03:18 -0500

    Practica - parte2

--- src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
+++ src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
@@ -5,22 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("hello")
+//@RequestMapping("hello")
+@RequestMapping(value = "hello")
 public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
         
-        @GetMapping("/{firstName}/{lastName}")
+        //Primera parte del taller - reto
+        /*@GetMapping("/{firstName}/{lastName}")
         public String sayHello(
                 @PathVariable("firstName") String firstName,
                 @PathVariable("lastName") String lastName) {
             return String.format("<h1> Hello  %S %S </h1>", firstName, lastName);
+        }*/
+        
+        //segunda parte del Taller dia1 - reto
+        @GetMapping("/{nombre}/{apellido}")
+        public String hello(@PathVariable("nombre") String nombre, @PathVariable("apellido") String apellido){
+            return "Hello" + nombre + " " + apellido;
+        }
+        
+        @GetMapping("/{nombre}/{apellido}")
+        public String helloMundo(@RequestParam("nombre") String nombre, @RequestParam("apellido") String apellido){
+            return "Hello" + nombre + " " + apellido;
         }
 
 } 
